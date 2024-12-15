b76a56f84663767d50d0980bc045ef589ebc807f
yun <qqeew1234@gmail.com>
2024-12-15 23:24:17 +0900

    진행불가

--- src/main/java/racing/Application.java
+++ src/main/java/racing/Application.java
@@ -3,50 +3,70 @@ package racing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static racing.Car.winCars;
 
 public class Application {
 
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
 
-    public static void main(Car[] args) {
+    public static void main(String[] args) {
 
-        ArrayList<Car> 차량이름목록 = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        // 3번 반복 받는 부분,
-        for (int i = 0; i < 3; i++) {
-            System.out.print("차량 이름을 입력해 주세요: ");
-            String 차이름 = scanner.nextLine();
+        int 이동예정횟수 = 10;
 
-            //10자초과 오류출력
-            if (차이름.length()>10){
-                throw new IllegalArgumentException("10글자이상 불가");
-            }
+        List<String> 차량이름목록 = 차량리스트입력();
+        toCars(차량이름목록) = 차량리스트입력();
 
-            차량이름목록.add(new Car(차이름));
+
+
+
+
+//        List<Car> 차량이동횟수 = List.of(new Car(0, "현대"), new Car(0, "테슬라"), new Car(0, "포르쉐"));
+
+        Car 자동차A = 차량이동횟수.get(0);
+        Car 자동차B = 차량이동횟수.get(1);
+        Car 자동차C = 차량이동횟수.get(2);
+        for (int i = 0; i < 이동예정횟수; i++) {
+            자동차A.전진();
+            System.out.println("A이동횟수 = " + 자동차A.이동횟수);
+
+
+            자동차B.전진();
+            System.out.println("B이동횟수 = " + 자동차B.이동횟수);
+
+
+            자동차C.전진();
+            System.out.println("C이동횟수 = " + 자동차C.이동횟수);
+
+
         }
 
-
-        //주사위 굴릴 횟수 입력
-        System.out.println("횟수를 입력해 주세요: ");
-        int count = Integer.parseInt(scanner.nextLine());
-
-        DiceGame game = new DiceGame();
-        for (int i = 0; i < count; i++) {
-            game.play(count);
-
-            //자사위에 따라 이동
-            for (Car.차이름)
-
-        }
-
-
+        List<Car> 이동횟수목록 = winCars(차량이동횟수);
+        System.out.println("우승자 = "+이동횟수목록);
 
     }
+
+    private static List<String> toCars(List<Car> 차량이름목록) {
+        List<String> 차량이름목록 = 차량리스트입력();
+        List<Car> 차량목록 = toCars(차량이름목록);
+    }
+
+    private static List<String> 차량리스트입력() {
+
+        System.out.print("차량 이름 : ");
+        String 차이름 = scanner.nextLine();
+
+        String[] 이름배열 = 차이름.split(",");
+        List<String> 이름리스트 = List.of(이름배열);
+
+        return 이름리스트;
+    }
+
 
 
 
--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -1,32 +1,79 @@
 package racing;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
-import java.util.Random;
-import java.util.Scanner;
+
+import static racing.Application.random;
 
 public class Car {
+    @Override
+    public String toString() {
+        return "Car{" +
+                "차량이름='" + 차량이름 + '\'' +
+                ", 이동횟수=" + 이동횟수 +
+                '}';
+    }
 
-    String 차이름;
+    String 차량이름;
     int 이동횟수;
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return 이동횟수 == car.이동횟수 && Objects.equals(차이름, car.차이름);
+        return 이동횟수 == car.이동횟수 && Objects.equals(차량이름, car.차량이름);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(차이름, 이동횟수);
+        return Objects.hash(차량이름, 이동횟수);
+    }
+
+    public Car(int 이동횟수, String 차량이름) {
+        this.이동횟수 = 이동횟수;
+        this.차량이름 = 차량이름;
     }
 
     public Car(int 이동횟수) {
         this.이동횟수 = 이동횟수;
     }
 
-    public Car(String 차이름) {
-        this.차이름 = 차이름;
+    public Car(String 차량이름) {
+        this.차량이름 = 차량이름;
     }
+
+
+    static List<Car> winCars(List<Car> 우승자목록) {
+        List<Integer> 이동횟수목록 = 우승자목록.stream()
+                .map(car -> car.이동횟수)
+                .toList();
+
+        int 최대이동 = Collections.max(이동횟수목록);
+
+        ArrayList<Car> cars1 = new ArrayList<>();
+        for (int i = 0; i < 우승자목록.size(); i++) {
+            Car 자동차 = 우승자목록.get(i);
+            if (자동차.이동횟수 == 최대이동) {
+                cars1.add(자동차);
+            }
+        }
+
+        return 우승자목록;
+    }
+
+    void 전진() {
+        int 주사위값 = random.nextInt(6) + 1;
+        if (주사위값 >= 3) {
+            이동횟수 = 이동횟수 + 1;
+        }
+
+
+    }
+
 }
+
+
+
 
